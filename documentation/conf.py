53cb640bd6e566c2efb3dbf81240245801cc5e3f
Brulport Fabien <fabien.brulport@epfl.ch>
2017-11-26 23:08:54 +0100

    Doc fonctionnelle, suppression de Weather.py

--- documentation/conf.py
+++ documentation/conf.py
@@ -85,7 +85,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle' #alabaster
+html_theme = 'alabaster' #alabaster #bizstyle
 html_logo = 'IBOAT_logo1.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
